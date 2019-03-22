935c37266fce2449992f89a1bbade6a3626d9036
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 15:32:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
     
 end
 
