c2eedc010993c18447c8c9c5837adae11660635f
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 15:40:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,14 +62,9 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
-	programmer_hash = 
- 		{   :yukihiro_matsumoto => {
+	programmer_hash = {   
+	  :yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
        },
@@ -109,7 +104,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
