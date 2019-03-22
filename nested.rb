8355bb9472f1f98e8e44080fda80aeea9dd0bf39
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 15:37:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,11 +69,10 @@ def adding_matz
 # }
 
 	programmer_hash = 
-	# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
- 		{
+ 		{   :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+       },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
